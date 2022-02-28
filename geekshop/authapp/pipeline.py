5a09ac8f3b6ac06603353361b6dd24c89658338a
GDI <dimon.1982@mail.ru>
2022-02-28 21:49:03 +0300

    part 2 lesson 3 add all excluded files to ordersapp

---
+++ geekshop/authapp/pipeline.py
@@ -0,0 +1,51 @@
+from collections import OrderedDict
+from datetime import datetime
+from urllib.parse import urlencode, urlunparse
+
+import requests
+from django.utils import timezone
+from social_core.exceptions import AuthForbidden
+
+from authapp.models import ShopUserProfile
+
+
+def save_user_profile(backend, user, response, *args, **kwargs):
+    if backend.name != 'vk-oauth2':
+        return
+
+    api_url = urlunparse(('https',
+                          'api.vk.com',
+                          '/method/users.get',
+                          None,
+                          urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about')),
+                                                access_token=response['access_token'],
+                                                v='5.92')),
+                          None
+                          ))
+
+    resp = requests.get(api_url)
+    if resp.status_code != 200:
+        return
+
+    data = resp.json()['response'][0]
+    if data['sex'] == 1:
+        user.shopuserprofile.gender = ShopUserProfile.FEMALE
+    elif data['sex'] == 2:
+        user.shopuserprofile.gender = ShopUserProfile.MALE
+    else:
+        user.shopuserprofile.gender = ShopUserProfile.NON_BINARY
+
+    if data['about']:
+        user.shopuserprofile.aboutMe = data['about']
+    user.save()
+"""
+    if data['bdate']:
+        bdate = datetime.strptime(data['bdate'], '%d.%m.%Y').date()
+
+        age = timezone.now().date().year - bdate.year
+        if age < 18:
+            user.delete()
+            raise AuthForbidden('social_core.backends.vk.VKOAuth2')
+"""
+
+
---
+++ geekshop/ordersapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from ordersapp.views import OrderList, OrderDelete, OrderCreate, OrderDetail, OrderUpdate, order_forming_complete
+
+app_name = 'ordersapp'
+urlpatterns = [
+
+    path('', OrderList.as_view(), name='list'),
+    path('create/', OrderCreate.as_view(), name='create'),
+    path('update/<int:pk>/', OrderUpdate.as_view(), name='update'),
+    path('read/<int:pk>/', OrderDetail.as_view(), name='read'),
+    path('delete/<int:pk>/', OrderDelete.as_view(), name='delete'),
+    path('forming_complete/<int:pk>/', order_forming_complete, name='forming_complete'),
+
+]
