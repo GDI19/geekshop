fddfc895c9f0fc9b34d08e900cd2ca59291dcbcb
GDI <dimon.1982@mail.ru>
2022-05-27 00:51:54 +0300

    part 2 lesson 6 add method select_related

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -88,7 +88,8 @@ class OrderUpdate(UpdateView):
         if self.request.POST:
             formset = OrderFormSet(self.request.POST, instance=self.object)
         else:
-            formset = OrderFormSet(instance=self.object)
+            queryset = self.object.orderitems.select_related()
+            formset = OrderFormSet(instance=self.object, queryset=queryset)
             for form in formset.forms:
                 if form.instance.pk:
                     form.initial['price'] = form.instance.product.price
