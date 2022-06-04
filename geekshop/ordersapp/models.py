10094ded7c1d38d589d738611a0bb97818e0a727
GDI <dimon.1982@mail.ru>
2022-06-04 19:17:13 +0300

    part 2 lesson 7 add tag {% with %}

--- geekshop/ordersapp/models.py
+++ geekshop/ordersapp/models.py
@@ -56,6 +56,14 @@ class Order(models.Model):
         self.is_active = False
         self.save()
 
+    def get_summary(self):
+        items = self.orderitems.select_related()
+        return {
+            'get_total_cost': sum(list(map(lambda x: x.get_product_cost(), items))),
+            'get_total_quantity': sum(list(map(lambda x: x.quantity, items)))
+        }
+
+
 
 """
 class OrderItemQuerySet(models.QuerySet):
@@ -69,9 +77,9 @@ class OrderItemQuerySet(models.QuerySet):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order,verbose_name='заказ',related_name='orderitems',on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,verbose_name='продукты',on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name='количество',default=0)
+    order = models.ForeignKey(Order, verbose_name='заказ', related_name='orderitems', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, verbose_name='продукты', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
 
     # objects = OrderItemQuerySet.as_manager()
 
