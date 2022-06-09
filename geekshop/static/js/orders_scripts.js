6e0f2465f65b0523e9d27cd486bf62cf3313d79d
GDI <dimon.1982@mail.ru>
2022-06-09 21:55:16 +0300

    part 2 lesson 7 change orders_scripts.js to previous class Total_forms

--- geekshop/static/js/orders_scripts.js
+++ geekshop/static/js/orders_scripts.js
@@ -2,10 +2,10 @@ window.onload = function () {
     let quantity, price, orderitem_num, delta_quantity, orderitem_quantity, delta_cost;
     let quantity_arr = []
     let price_arr = []
-    let total_forms = parseInt($('input[name=orderitems-TOTAL_FORMS]').val())
+    let TOTAL_FORMS = parseInt($('input[name=orderitems-TOTAL_FORMS]').val())
     let order_total_quantity = parseInt($('.order_total_quantity').text()) || 0;
     let order_total_cost = parseInt($('.order_total_cost').text().replace(',', '.')) || 0;
-    for (let i = 0; i < total_forms; i++) {
+    for (let i = 0; i < TOTAL_FORMS; i++) {
         quantity = parseInt($('input[name=orderitems-' + i + '-quantity]').val())
         price = parseInt($('.orderitems-' + i + '-price').text().replace(',', '.'))
         quantity_arr[i] = quantity;
